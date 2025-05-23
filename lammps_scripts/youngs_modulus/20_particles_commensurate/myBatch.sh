0f3e4f378882371500278e5307902c79e7a030b1
Hanna Falch Næstby <hannanaestby@dhcp-10-22-132-52.wlan.ntnu.no>
2025-05-23 17:19:37 +0200

    Youngs Modulus

---
+++ lammps_scripts/youngs_modulus/20_particles_commensurate/myBatch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=chy40inc          # Job name
+#SBATCH --output=lammps_output.log     # Output log file
+#SBATCH --error=lammps_error.log       # Error log file
+#SBATCH --nodes=1                      # Number of nodes
+#SBATCH --ntasks-per-node=4            # Number of tasks per node (adjust as needed)
+#SBATCH --time=15:00:00                # Time limit (hh:mm:ss) adjust this based on your job's needs
+#SBATCH --partition=porelab        # Partition/queue (replace with appropriate partition name)
+
+# Load necessary modules
+module load eb/use.eb
+module load GCC/11.2.0
+module load OpenMPI/4.1.1
+module load LAMMPS/23Jun2022-kokkos
+
+# Run the LAMMPS job
+srun lmp -in run.in
